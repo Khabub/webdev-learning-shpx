bed1c0c6a1916c098ca47bb0d614e5a3ae701997
Robert Rozehnal <rob.kab.roz@gmail.com>
2022-07-10 21:51:06 +0200

    Set MainWindow

---
+++ src/components/Layout/NavigationPanel/NavigationPanel_styles.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 50px;
+  width: 100vw;
+  position: fixed;
+  top: 0;  
+  color: #555;
+  background-color: #fff;
+  z-index: 10;
+
+  h1 {
+    margin-left: 1rem;
+  }
+
+  img {
+    margin-right: 1rem;
+  }
+
+
+`;
